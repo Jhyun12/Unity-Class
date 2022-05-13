26898d292dd798e9fa95786d1b146f08f11010eb
Jhyun12 <105360218+Jhyun12@users.noreply.github.com>
2022-05-13 21:51:53 +0900

    deltaTime & ObjectMovement
    * Time.deltaTime
    
    public GameObject target
    private float speed = 1.0f
    
    transform.position = Vector3.MoveTowards
                (
                transform.position,
                target.transform.position,
                speed * Time.deltaTime
                )

--- My project/Assets/Script/NewBehaviourScript.cs
+++ My project/Assets/Script/NewBehaviourScript.cs
@@ -13,6 +13,27 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        Debug.Log(Time.deltaTime);
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.forward * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.back * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * Time.deltaTime;
+        }
     }
 }
