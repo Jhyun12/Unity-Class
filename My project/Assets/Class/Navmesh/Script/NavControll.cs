1a8aafbd043808a8eead9d2ae5490e8e7f2afe5b
Jhyun12 <105360218+Jhyun12@users.noreply.github.com>
2022-05-24 21:47:26 +0900

    NavMesh....

---
+++ My project/Assets/Class/Navmesh/Script/NavControll.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavControll : MonoBehaviour
+{
+    public float speed;
+    NavMeshAgent agent;
+
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit hit;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if(Physics.Raycast(ray,out hit, Mathf.Infinity))
+            {
+                Move(hit.point);
+            }
+        }
+    }
+
+    public void Move(Vector3 direction)
+    {
+        agent.speed = speed;
+        agent.SetDestination(direction);
+    }
+}
